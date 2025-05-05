138767302696947dd3743fbccfd43e6d8aab9afe
vishwajeet161 <vishwajeet.patel161@gmail.com>
2025-05-06 00:44:25 +0530

    created localDb files

--- app/src/main/java/org/ticketBooking/services/UserBookingService.java
+++ app/src/main/java/org/ticketBooking/services/UserBookingService.java
@@ -1,4 +1,13 @@
 package org.ticketBooking.services;
 
+import org.ticketBooking.entities.User;
+
 public class UserBookingService {
+    private User user;
+
+    public UserBookingService(User user){
+        this.user = user;
+
+  ;  }
+
 }
